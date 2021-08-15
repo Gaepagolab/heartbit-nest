7a6742146c628f684f7d51ca929b352221fdb3fa
Yongjoo Choi <yongjoo.choi@nearthlab.com>
2021-08-15 15:34:45 +0900

    test: authentication integrations

---
+++ src/utils/mocks/user.mock.ts
@@ -0,0 +1,10 @@
+import User from '../../users/user.entity';
+
+const mockedUser: User = {
+  id: 1,
+  email: 'test@test.com',
+  name: 'test',
+  password: 'hash',
+};
+
+export default mockedUser;
