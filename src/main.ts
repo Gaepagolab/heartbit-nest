ee09e179638965ec2184e519130d2bc28c1edba0
Yongjoo Choi <yongjoo.choi@nearthlab.com>
2022-02-20 20:01:27 +0900

    add helmet

--- src/main.ts
+++ src/main.ts
@@ -1,18 +1,21 @@
-import { NestFactory, Reflector } from '@nestjs/core';
+import { NestFactory } from '@nestjs/core';
 import * as cookieParser from 'cookie-parser';
-import { ValidationPipe, ClassSerializerInterceptor } from '@nestjs/common';
+import * as helmet from 'helmet';
 
 import { AppModule } from './app.module';
 import { activateSwaggerModule } from './utils/swagger/activate-swagger';
+import { LoggingInterceptor } from './core/interceptors/logging.interceptor';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
+
   activateSwaggerModule(app);
 
+  app.useGlobalInterceptors(new LoggingInterceptor());
   app.enableCors({ credentials: true, origin: true });
-  app.useGlobalPipes(new ValidationPipe());
-  app.useGlobalInterceptors(new ClassSerializerInterceptor(app.get(Reflector)));
   app.use(cookieParser());
+  app.use(helmet());
+
   await app.listen(3000);
 }
 bootstrap();
