9fd22e53274582e21246deed12bde2d0fe67b26c
Yongjoo Choi <yongjoo.choi@nearthlab.com>
2022-02-20 22:13:14 +0900

    add: post candle protocol

--- src/modules/candles/candles.controller.ts
+++ src/modules/candles/candles.controller.ts
@@ -1,12 +1,12 @@
 import { Body, Post } from '@nestjs/common';
 
 import { SwaggerController } from '../../core/decorators/controller.decorator';
-import { CreateCandleBody } from './protocols/create-candle.body';
+import { PostCandleBody } from './protocols/post-candle.body';
 
 @SwaggerController('core', '[core] candle')
 export class CandlesController {
   @Post('/candle')
-  async post(@Body() createCandleBody: CreateCandleBody): Promise<any> {
-    return createCandleBody;
+  async post(@Body() body: PostCandleBody): Promise<any> {
+    return body;
   }
 }
--- src/modules/candles/protocols/create-candle.body.ts
+++
@@ -1,8 +0,0 @@
-import { IntersectionType, PickType } from '@nestjs/swagger';
-import { CandleOptionalPropertiesSwagger } from './candle.optional-properties.swagger';
-import { CandleRequiredPropertiesSwagger } from './candle.required-properties.swagger';
-
-export class CreateCandleBody extends IntersectionType(
-  PickType(CandleRequiredPropertiesSwagger, ['type', 'coinId', 'ohldvs']),
-  PickType(CandleOptionalPropertiesSwagger, undefined),
-) {}
