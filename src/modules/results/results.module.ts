bff689b4a13adbef01197c745ad4141bbbb5e71a
Yongjoo Choi <yongjoo.choi@nearthlab.com>
2022-03-06 16:37:14 +0900

    add module

---
+++ src/modules/results/results.module.ts
@@ -0,0 +1,9 @@
+import { Module } from '@nestjs/common';
+import { TypeOrmModule } from '@nestjs/typeorm';
+
+import ResultEntity from './result.entity';
+
+@Module({
+  imports: [TypeOrmModule.forFeature([ResultEntity])],
+})
+export class ResultsModule {}
