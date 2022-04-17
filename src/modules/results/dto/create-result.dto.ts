3ad050e5183d16a875546c4f6c9c5e1c05637f3f
Yongjoo Choi <yongjoo.choi@nearthlab.com>
2022-04-17 18:07:54 +0900

    add protocol

--- src/modules/results/dto/create-result.dto.ts
+++ src/modules/results/dto/create-result.dto.ts
@@ -7,9 +7,8 @@ export class CreateResultDto {
     findEnd: string;
   }): CreateResultDto {
     const it = new CreateResultDto();
-    const { candleId, currentStart, currentEnd, findStart, findEnd } = options;
+    const { currentStart, currentEnd, findStart, findEnd } = options;
 
-    it.candleId = candleId;
     it.currentStart = currentStart;
     it.currentEnd = currentEnd;
     it.findStart = findStart;
