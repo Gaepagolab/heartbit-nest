232ebee4e62806e37cb698be506ff2fca88b1e33
Yongjoo Choi <yongjoo.choi@nearthlab.com>
2022-05-15 16:33:16 +0900

    fix query protocol

--- src/modules/finances/protocols/finance.query.ts
+++ src/modules/finances/protocols/finance.query.ts
@@ -1,11 +1,12 @@
 import { IntersectionType, PickType } from '@nestjs/swagger';
 import { FinanceFilterDto } from '../dtos/finance.filter.dto';
 import { FinanceOptionalPropertiesSwagger } from './finance.optional-properties.swagger';
+import { FinanceRequiredPropertiesSwagger } from './finance.requried-properties.swagger';
 
 export class FinanceQuery extends IntersectionType(
   // line break
   PickType(FinanceOptionalPropertiesSwagger, ['type']),
-  PickType(FinanceOptionalPropertiesSwagger, []),
+  PickType(FinanceRequiredPropertiesSwagger, []),
 ) {
   toFinanceFilterDto(): FinanceFilterDto {
     return FinanceFilterDto.from({ ...this });
